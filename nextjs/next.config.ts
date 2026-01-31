c95b4e83abd8e3cd7315cfb2e9c5fc14315b8e95
wilkax <axel.wilkening@gmail.com>
2026-01-31 23:31:07 +0100

    Root Dir correction

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -3,7 +3,8 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingRoot: path.join(__dirname),
+  // Set the root to parent directory since Vercel root directory is set to /nextjs
+  outputFileTracingRoot: path.join(__dirname, ".."),
 };
 
 export default nextConfig;
